1b38c3b7d537483d0d767a0f3fd632f3aceb6b6a
Alexis Manuel <frsknalexis.1997@gmail.com>
2022-02-14 00:57:53 -0500

    [SPA control in backend]

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -20,6 +21,10 @@ app.use('/admin-app/v1/hospitales', require('./routes/hospitales'));
 app.use('/admin-app/v1/medicos', require('./routes/medicos'));
 app.use('/admin-app/v1/search', require('./routes/searchs'));
 app.use('/admin-app/v1/upload', require('./routes/uploads'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Server run on port: ' + process.env.PORT);
